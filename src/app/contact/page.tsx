f2faa90ca31a05b1870a1db02b764c43b19a8d02
yewon97 <dpdnjs9936@gmail.com>
2024-01-02 00:21:12 +0900

    client side form 만들기

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,34 @@
+import ContactForm from "@/components/ContactForm";
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+
+const LINKS = [
+  { icon: <AiFillGithub />, url: "" },
+  { icon: <AiFillLinkedin />, url: "" },
+  { icon: <AiFillYoutube />, url: "" },
+];
+
 export default function ContactPage() {
-	return (
-		<p>연락하기</p>
-	)
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>dpdnjs402@naver.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, idx) => {
+          return (
+            <li key={idx}>
+              <a
+                href={link.url}
+                target="_blank"
+                className="text-5xl hover:text-yellow-500"
+              >
+                {link.icon}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <h2>Or Send me an Email</h2>
+      <ContactForm />
+    </section>
+  );
 }
