aa9281349885ad5d008c03a45a75f5b1ea124708
yewon97 <dpdnjs9936@gmail.com>
2024-01-01 22:04:04 +0900

    이전글 다음글 post 페이지에서 임시로 보여주기

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,6 @@
-import PostContent from '@/components/PostContent';
-import { getPostData } from '@/service/posts';
-import Image from 'next/image';
+import PostContent from "@/components/PostContent";
+import { getPostData } from "@/service/posts";
+import Image from "next/image";
 
 type Props = {
   params: {
@@ -10,17 +10,24 @@ type Props = {
 
 export default async function PostPage({ params: { slug } }: Props) {
   const post = await getPostData(slug);
+  const { title, path, next, prev } = post;
+  console.log("prev: ", prev);
+  console.log("next: ", next);
 
   return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
       <Image
         className="w-full h-1/5 max-h-[500px]"
-        src={`/images/posts/${post.path}.png`}
-        alt={post.title}
+        src={`/images/posts/${path}.png`}
+        alt={title}
         width={760}
         height={420}
       />
       <PostContent post={post} />
+      <section>
+        {prev && <p>{prev.title}</p>}
+        {next && <p>{next.title}</p>}
+      </section>
     </article>
   );
 }
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,5 +1,5 @@
-import { readFile } from 'fs/promises';
-import path from 'path';
+import { readFile } from "fs/promises";
+import path from "path";
 
 export type Post = {
   title: string;
@@ -27,14 +27,14 @@ export async function getNonFeaturedPosts(): Promise<Post[]> {
 }
 
 export async function getAllPosts(): Promise<Post[]> {
-  const filePath = path.join(process.cwd(), 'data', 'posts.json');
-  return readFile(filePath, 'utf-8')
+  const filePath = path.join(process.cwd(), "data", "posts.json");
+  return readFile(filePath, "utf-8")
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
 }
 
 export async function getPostData(fileName: string): Promise<PostData> {
-  const filePath = path.join(process.cwd(), `data`, 'posts', `${fileName}.md`);
+  const filePath = path.join(process.cwd(), `data`, "posts", `${fileName}.md`);
   const posts = await getAllPosts();
   const post = posts.find((post) => post.path === fileName);
 
@@ -42,7 +42,7 @@ export async function getPostData(fileName: string): Promise<PostData> {
 
   const index = posts.indexOf(post);
   const next = index > 0 ? posts[index - 1] : null;
-  const prev = index < posts.length ? posts[index + 1] : null;
-  const content = await readFile(filePath, 'utf-8');
+  const prev = index < posts.length - 1 ? posts[index + 1] : null;
+  const content = await readFile(filePath, "utf-8");
   return { ...post, content, next, prev };
 }
